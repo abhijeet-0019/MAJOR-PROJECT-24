d43d226d17ae83f4201cfa6fc6f587abc64069e9
abhijeet0019 <abhijeetsinghbhainser@gmail.com>
2024-05-18 10:49:09 +0530

    calendar and student list with filters

--- app/admin/calendar/page.js
+++ app/admin/calendar/page.js
@@ -1,11 +1,48 @@
-import React from 'react'
+'use client'
+import React, { useState, useEffect } from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import useAPIData from '../../../apiConfig/useAPIData';
 
-const Calendar = () => {
-  return (
-    <div>
-      <h1>Calendar</h1>
+export default function CalendarWithEvents() {
+  const [events, setEvents] = useState([]);
+  const { getItems } = useAPIData();
+
+  useEffect(() => {
+    async function fetchEvents() {
+      try {
+        const academicDetailsResponse = await getItems('TPO_Drive', null, null, null, null, null, null, true);
+        
+        const formattedEvents = academicDetailsResponse.data.map(event => ({
+          title: event.Name, 
+          start: new Date(event.StartDate), 
+          
+        }));
+
+        setEvents(formattedEvents);
+      } catch (error) {
+        console.error('Error fetching events:', error);
+      }
+    }
+
+    fetchEvents();
+  }, []); 
+
+  const renderEventContent = (eventInfo) => (
+    <div style={{ padding: '5px', borderRadius: '4px' }}>
+      <b>{eventInfo.event.title}</b>
     </div>
-  )
-}
+  );
 
-export default Calendar
+  return (
+    <div className="calendar-container" style={{ padding: '40px', paddingTop: '20px', backgroundColor: '#dddfeb' }}>
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        events={events}
+        eventContent={renderEventContent}
+        eventDisplay="title"      
+/>
+    </div>
+  );
+}
--- app/admin/students/page.js
+++ app/admin/students/page.js
@@ -1,11 +1,255 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  Button,
+  Grid,
+} from "@mui/material";
+import useAPIData from "../../../apiConfig/useAPIData";
 
-const StudentList = () => {
+const StudentTable = () => {
+  const [originalStudents, setOriginalStudents] = useState([]);
+  const [filteredStudents, setFilteredStudents] = useState([]);
+  const [filters, setFilters] = useState({
+    branchId: "",
+    graduationYear: "",
+    rollNoMin: "",
+    rollNoMax: "",
+    bePercentage: "",
+  });
+  const [shouldApplyFilters, setShouldApplyFilters] = useState(false); // State to track whether filters should be applied
+  const { getItems } = useAPIData();
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const academicDetailsResponse = await getItems(
+        "TPO_academic_details",
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        true
+      );
+      const academicDetails = academicDetailsResponse.data;
+
+      const personalDetailsResponse = await getItems(
+        "TPO_students_personal_details",
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        true
+      );
+      const personalDetails = personalDetailsResponse.data;
+
+      const joinedStudents = academicDetails.map((academic) => {
+        const personal = personalDetails.find(
+          (personal) => personal.user_id === academic.user_id
+        );
+        return {
+          ...academic,
+          name: personal ? personal.name : "Unknown",
+          gender: personal ? personal.gender : "Unknown",
+        };
+      });
+
+      setOriginalStudents(joinedStudents);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const applyFilters = () => {
+    let filteredData = originalStudents.filter((student) => {
+      let pass = true;
+      if (filters.branchId && student.branch_id !== filters.branchId)
+        pass = false;
+      if (
+        filters.graduationYear &&
+        student.grad_year !== parseInt(filters.graduationYear)
+      )
+        pass = false;
+      if (filters.rollNoMin && student.roll_no < parseInt(filters.rollNoMin))
+        pass = false;
+      if (filters.rollNoMax && student.roll_no > parseInt(filters.rollNoMax))
+        pass = false;
+      if (
+        filters.bePercentage &&
+        student.be_percentage <= parseInt(filters.bePercentage)
+      )
+        pass = false;
+      return pass;
+    });
+    setFilteredStudents(filteredData);
+  };
+  const clearFilters = () => {
+    setFilters({
+      branchId: "",
+      graduationYear: "",
+      rollNoMin: "",
+      rollNoMax: "",
+      bePercentage: "",
+    });
+    setShouldApplyFilters(false);
+    setFilteredStudents(originalStudents);
+  };
+
+  const handleFilterChange = (event) => {
+    const { name, value } = event.target;
+    setFilters({
+      ...filters,
+      [name]: value,
+    });
+  };
+
+  const handleFilterSubmit = () => {
+    applyFilters();
+    setShouldApplyFilters(true); // Set shouldApplyFilters to true when button is clicked
+  };
+
   return (
-    <div>
-      <h1>All the Students</h1>
-    </div>
-  )
-}
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Paper>
+          <Grid
+            container
+            spacing={2}
+            alignItems="center"
+            justifyContent="space-between"
+            style={{ padding: "16px" }}
+          >
+            <Grid item>
+              <TextField
+                name="branchId"
+                label="Branch ID"
+                value={filters.branchId}
+                onChange={handleFilterChange}
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                name="graduationYear"
+                label="Graduation Year"
+                value={filters.graduationYear}
+                onChange={handleFilterChange}
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                name="rollNoMin"
+                label="Roll No Min"
+                value={filters.rollNoMin}
+                onChange={handleFilterChange}
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                name="rollNoMax"
+                label="Roll No Max"
+                value={filters.rollNoMax}
+                onChange={handleFilterChange}
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                name="bePercentage"
+                label="BE Percentage"
+                value={filters.bePercentage}
+                onChange={handleFilterChange}
+              />
+            </Grid>
+            <Grid item>
+              <Button
+                variant="contained"
+                onClick={handleFilterSubmit}
+                sx={{
+                  backgroundColor: 'primary.main',
+                  color: 'black',
+                  '&:hover': {
+                    backgroundColor: 'primary.dark',
+                    color: 'white',
+                  },
+                }}
+              >
+                Apply Filters
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                variant="contained"
+                onClick={clearFilters}
+                sx={{
+                  backgroundColor: 'primary.main', // change the background color to primary.main
+                  color: 'black', // change the text color to black
+                  '&:hover': {
+                    backgroundColor: 'primary.dark', // change the background color on hover to primary.dark
+                    color: 'white', // change the text color on hover to white
+                  },
+                }}
+              >
+                Clear Filters
+              </Button>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Grid>
+      <Grid item xs={12}>
+        <Paper>
+          <TableContainer>
+            <Table aria-label="student table">
+              <TableHead style={{ backgroundColor: "#c1c0cf" }}>
+                <TableRow>
+                  <TableCell style={{ fontWeight: "bold" }}>Name</TableCell>
+                  <TableCell style={{ fontWeight: "bold" }}>Roll No</TableCell>
+                  <TableCell style={{ fontWeight: "bold" }}>
+                    Graduation Year
+                  </TableCell>
+                  <TableCell style={{ fontWeight: "bold" }}>
+                    {" "}
+                    BE Percentage
+                  </TableCell>
+                  <TableCell style={{ fontWeight: "bold" }}>
+                    {" "}
+                    Branch ID
+                  </TableCell>
+                  <TableCell style={{ fontWeight: "bold" }}>Gender</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {(shouldApplyFilters ? filteredStudents : originalStudents).map(
+                  (student) => (
+                    <TableRow key={student.id}>
+                      <TableCell>{student.name}</TableCell>
+                      <TableCell>{student.roll_no}</TableCell>
+                      <TableCell>{student.grad_year}</TableCell>
+                      <TableCell>{student.be_percentage}</TableCell>
+                      <TableCell>{student.branch_id}</TableCell>
+                      <TableCell>{student.gender}</TableCell>
+                    </TableRow>
+                  )
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+};
 
-export default StudentList
+export default StudentTable;
